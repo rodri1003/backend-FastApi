bac88c96c16b095e8a237d0d8ef54fb3787c8c60
Rodrigo Mejia <rodrigomejia964@gmail.com>
2026-02-21 17:38:44 -0600

    Agrega CORS en main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.db.session import Base, engine
 from app.models.user import User
 
@@ -7,6 +9,14 @@ from app.api.auth import router as auth_router
 from app.api.admin import router as admin_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173","http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
